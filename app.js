1659edbe955a50a1862c1160e767e3334ee288de
jinwook song <wlsdnr129@naver.com>
2022-08-01 22:19:23 +0900

    complete painting board

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+const clearBtn = document.getElementById('clear-canvas');
+const toggleMode = document.getElementById('toggle-mode');
 const colorOptions = [...document.getElementsByClassName('color-option')];
 const selectedColor = document.getElementById('color');
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const CANVAS_SIZE = innerWidth * 0.4;
 
+// Canvas Set
+const CANVAS_SIZE = innerWidth * 0.4;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 ctx.lineWidth = lineWidth.value;
 
+// Paint Mode
 let isPainting = false;
+let isFilling = false;
 
+// Drawing Functions
 function onMove({ offsetX, offsetY }) {
   if (isPainting) {
     ctx.lineTo(offsetX, offsetY);
@@ -27,6 +33,7 @@ function cancelPainting() {
   ctx.beginPath();
 }
 
+// Set Options
 function setColor(color) {
   ctx.strokeStyle = color;
   ctx.fillStyle = color;
@@ -45,15 +52,38 @@ function handleColorPick({
 function handleLineWidth({ target: { value } }) {
   ctx.lineWidth = value;
 }
+function handleToggleMode() {
+  if (isFilling) {
+    isFilling = false;
+    toggleMode.innerText = 'FILL';
+  } else {
+    isFilling = true;
+    toggleMode.innerText = 'DRAW';
+  }
+}
 
+// Fill & Clear
+function handleFillCanvas() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+function handleClearCanvas() {
+  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+// Event Listeners
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
+canvas.addEventListener('click', handleFillCanvas);
 
 selectedColor.addEventListener('change', handleColorChange);
 colorOptions.forEach((color) =>
   color.addEventListener('click', handleColorPick)
 );
-
 lineWidth.addEventListener('change', handleLineWidth);
+
+toggleMode.addEventListener('click', handleToggleMode);
+clearBtn.addEventListener('click', handleClearCanvas);
